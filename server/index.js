96cf8f80cdf47c38c5af3f4a9c755d0fdbcbedfe
karthikeyan <karthikeyana42004@gmail.com>
2026-01-04 15:14:49 +0530

    connection

--- server/index.js
+++ server/index.js
@@ -36,6 +36,7 @@ console.log("Express app created.");
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
+  "https://divakar.onrender.com",
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
